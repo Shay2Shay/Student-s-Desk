9ab36757c4f1b89afdae92c4702300d33a773aec
percyps2401 <onlystudies2424@gmail.com>
2022-11-19 23:32:30 +0530

    Added Person Interface

---
+++ Person/src/PersonInterface.java
@@ -0,0 +1,12 @@
+interface Person
+{
+    public  static String name;
+     public static int age;
+      public static String dob;
+    public static String gender;
+    public static String ID;
+    public static String address;
+
+
+
+}
